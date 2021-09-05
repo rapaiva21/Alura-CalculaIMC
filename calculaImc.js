4f6a4b818bf3ed6bd68d5ae9fffdba9f852ab2c3
Carlos Henrique <rapaiva21@gmail.com>
2021-09-04 22:38:03 -0300

    Adicionado o if para o peso marcação em caso de erro

--- calculaImc.js
+++ calculaImc.js
@@ -12,5 +12,13 @@ for (var i = 0; i < pacientes.length; i++){
     var tdImc = paciente.querySelector(".info-imc");
     var imc = peso / (altura * altura)
     tdImc.textContent = imc.toFixed(2);
-    
+
+    var pesoEhValido = true;
+    var alturaEhValido = true;
+
+    if (peso <= 0 || peso >= 500){
+        pesoEhValido = false;
+        console.log("Peso inválido");
+        paciente.classList.add("paciente-invalido");
+    }
 };
