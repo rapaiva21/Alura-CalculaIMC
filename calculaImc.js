2eca32f65d55174df3f43ff97e3a8a9bfe52a9a6
Carlos Henrique <rapaiva21@gmail.com>
2021-09-05 12:00:36 -0300

    Criação paras as funcionalidades do form.

--- calculaImc.js
+++ calculaImc.js
@@ -24,11 +24,49 @@ for (var i = 0; i < pacientes.length; i++){
         console.log("Altura inválido");
         paciente.classList.add("paciente-invalido");
     }
+
+    if (pesoEhValido && alturaEhValido){
+        var imc = calculaImc(peso, altura);
+        tdImc.textContent = calculaImc(imc);
+    }
 };
 
-var botaoAdicionar = document.querySelector("#form-adiciona");
+function calculaImc(peso, altura){
+    var imc = 0;
+    imc = peso / (altura * altura);
+    return imc.toFixed(2);
+}
+
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
 botaoAdicionar.addEventListener("click", function(event){
-    event.preventDefault()
-    console.log("teste do botaoAdicionar")
+    event.preventDefault();
+    console.log("teste do botaoAdicionar");
+    var form = document.querySelector("#form-adiciona");
+    nome = form.nome.value;
+    peso = form.peso.value;
+    altura = form.altura.value;
+    gordura = form.gordura.value;
+
+    var pacienteTr = document.createElement("tr");
+    var nomeTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");
+
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+    imcTd.textContent = calculaImc(peso, altura);
+        
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+    pacienteTr.appendChild(imcTd);
+
+    console.log(nomeTd);
+
 
 });
