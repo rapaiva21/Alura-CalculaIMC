a1be78e827afa9985b338c4bf94247290135f5df
Carlos Henrique <rapaiva21@gmail.com>
2021-09-08 09:29:10 -0300

    Adicionado erros de validação do form e outros functions.

--- form.js
+++ form.js
@@ -5,7 +5,23 @@ botaoAdicionar.addEventListener("click", function(event){
     var paciente = obtemPacienteDoFormulario(form);
 
     console.log(paciente);
+    
+
+    var erros = validaPaciente(paciente);
+    
+
+    if (erros.length > 0) {
+        exibeMensagensDeErro(erros);
+        return;
+    }
     var pacienteTr = montaTr(paciente);
+
+   
+    
+
+    var mensagensErro = document.querySelector("#mensagens-erro");
+    mensagensErro.innerHTML = "";
+
     form.reset();
 });
 
@@ -33,6 +49,8 @@ function montaTr(paciente){
 
     var tabela = document.querySelector("#tabela-pacientes");
     tabela.appendChild(pacienteTr);
+
+    
 }
 
 function montaTd(dado, classe){
@@ -41,4 +59,46 @@ function montaTd(dado, classe){
     td.classList.add(classe);
     
     return td;
+}
+
+function validaPaciente(paciente){
+
+    var erros = [];
+
+    if (paciente.nome.length == 0) {
+        erros.push("O nome não pode ser em branco");
+    }
+
+    if (paciente.gordura.length == 0) {
+        erros.push("A gordura não pode ser em branco");
+    }
+
+    if (paciente.peso.length == 0) {
+        erros.push("O peso não pode ser em branco");
+    }
+
+    if (paciente.altura.length == 0) {
+        erros.push("A altura não pode ser em branco");
+    }
+
+    if (!validaPeso(paciente.peso)) {
+        erros.push("Peso é inválido");
+    }
+
+    if (!validaAltura(paciente.altura)) {
+        erros.push("Altura é inválida");
+    }
+
+    return erros;
+}
+
+function exibeMensagensDeErro(erros) {
+    var ul = document.querySelector("#mensagens-erro");
+    ul.innerHTML = "";
+
+    erros.forEach(function(erro) {
+        var li = document.createElement("li");
+        li.textContent = erro;
+        ul.appendChild(li);
+    });
 }
