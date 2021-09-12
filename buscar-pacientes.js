0c8c740fc3948321eb98456d3295d10d7ae7ca5e
Carlos Henrique <rapaiva21@gmail.com>
2021-09-12 10:43:31 -0300

    Adicionado um if para encaminhar a busca para mensagem de erro

--- buscar-pacientes.js
+++ buscar-pacientes.js
@@ -7,15 +7,20 @@ botaoAdicionar.addEventListener("click" , function(){
     xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
 
     xhr.addEventListener("load" , function() {
-        
-        var resposta = xhr.responseText;
-        
-        var pacientes = JSON.parse(resposta);
-        console.log(pacientes);
 
-        pacientes.forEach(function(paciente) {
-            adicionaPacienteNaTabela(paciente);
-        });
+        var erroAjax = document.querySelector("#erro-ajax");
+        
+        if (xhr.status == 200) {
+            var resposta = xhr.responseText;
+            var pacientes = JSON.parse(resposta);
+
+            pacientes.forEach(function(paciente) {
+                adicionaPacienteNaTabela(paciente);
+            });
+        } else {
+            erroAjax.classList.remove("invisivel");
+        }
+                
     });
     xhr.send();
 });
